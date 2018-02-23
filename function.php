282dfba0262e38e5508ea78b2d38eb34f6387a54
shalulian <35538706+shalulian@users.noreply.github.com>
2018-02-23 13:19:22 +0700

    Update function.php

--- function.php
+++ function.php
@@ -113,12 +113,12 @@ function updatedragontiger($id,$column,$bet){
 	update($id,"dragontiger","เสมอ",0);
 	$n=0;
 	foreach($row as $i=>$v){
-		if($n>=7&&$i==$column&&($v==0||$v==$bet)){
-			$rtext="คุณ ".json_decode($row["Name"],true)." แทง $i $bet บาท";
+		if($n>=7&&$i==$column&&($row['เสือ']!=0||$row['มังกร']!=0||$row['เสมอ']!=0)){
+			$rtext="คุณ ".json_decode($row["Name"],true)." เปลี่ยนจากแทง $i $v บาท เป็นแทง $bet บาท";
 			update($id,"dragontiger",$i,$bet);
 		}
-		elseif($n>=7&&$v==$column&&$v!=$bet){
-			$rtext="คุณ ".json_decode($row["Name"],true)." เปลี่ยนจากแทง $i $v บาท เป็นแทง $bet บาท";
+		elseif($n>=7&&$i==$column&&($v==0||$v==$bet)){
+			$rtext="คุณ ".json_decode($row["Name"],true)." แทง $i $bet บาท";
 			update($id,"dragontiger",$i,$bet);
 		}
 		$n++;
