ba6efb5503e23d2c718f37abd60be4ee58d10a53
maxxvalue <32428979+maxxvalue@users.noreply.github.com>
2018-05-01 14:45:17 +0700

    Update dragontiger.php

--- dragontiger.php
+++ dragontiger.php
@@ -135,7 +135,7 @@ lineId:$lineid";
 					$manymessage=2;
 					update(1,$table,"status",2);
 					update(1,$table,"admin",1);
-					$replytext1='';
+					$replytext1='♠️♥️ขอให้เฮงๆรวย♦️♣️';
 					$replytext2="เปิดรอบที่ ".$lap;
 				}
 				else{
